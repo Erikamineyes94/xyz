4db146a8d53232211c9fc9a75bee00ecf07ade8a
Erikamineyes94 <melettamatta@gmail.com>
2019-12-09 12:29:43 +0100

    First commit

---
+++ Hello4/src/main/java/on/Hello4/App.java
@@ -0,0 +1,13 @@
+package on.Hello4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
